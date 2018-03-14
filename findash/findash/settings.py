5971966f23bc88a09156729801bed07923b9a82f
ohduran <alvaro.duranb@gmail.com>
2018-03-14 23:04:54 +0000

    PostgreSQL database created

--- findash/findash/settings.py
+++ findash/findash/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'findash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'findashdb',
+	'USER': 'findashuser',
+	'PASSWORD':'finda$h',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 

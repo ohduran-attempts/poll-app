bf67c54460177be76f28f3c60f5ab7ff73e54f00
ohduran <alvaro.duranb@gmail.com>
2018-03-16 18:46:10 +0000

    Set up everything for Templates to be created.

--- findash/polls/admin.py
+++ findash/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
+
+# telling the admin that Question objects have an admin interface.
+admin.site.register(Question)

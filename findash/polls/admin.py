22b5166d82d146aa735ef1b4815640d753ef9eb8
ohduran <alvaro.duranb@gmail.com>
2018-03-18 11:10:03 +0000

    Admin page and styling

--- findash/polls/admin.py
+++ findash/polls/admin.py
@@ -1,6 +1,29 @@
+"""Admin registry."""
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
-# telling the admin that Question objects have an admin interface.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):  # displayed as a table
+    """Adding choices when creating a Question."""
+
+    model = Choice
+    extra = 2  # number of minimum choices
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """Question Admin class."""
+
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    # Change list
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+admin.site.register(Question, QuestionAdmin)
--- findash/polls/models.py
+++ findash/polls/models.py
@@ -1,23 +1,36 @@
+"""Model suite."""
 from django.db import models
 from django.utils import timezone
-
 import datetime
-# Create your models here.
+
 
 class Question(models.Model):
+    """Question Model"""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self): # what will return print(Question)
+    def __str__(self):
+        """print Question."""
         return self.question_text
 
     def was_published_recently(self):
+        """Boolean for the question to appear in Index."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
 class Choice(models.Model):
+    """Choice Model."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
+        """print Choice."""
         return self.choice_text
